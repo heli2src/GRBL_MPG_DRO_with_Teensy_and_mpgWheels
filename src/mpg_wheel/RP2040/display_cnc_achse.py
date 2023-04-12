cd78470cbcc9ea563ab42076e47e2c5710b319a9
heli2src <jung@heli2.de>
2023-04-12 21:41:26 +0200

    WIP

--- src/mpg_wheel/RP2040/display_cnc_achse.py
+++ src/mpg_wheel/RP2040/display_cnc_achse.py
@@ -1,11 +1,12 @@
 '''
 Handwheel with Encoder + Rasperry Pi Pico
 
-   100 PPR Encoder  a -> GP18 Pin24
-                    b -> GP19 Pin25
+   100 PPR Encoder  a -> GP18 Pin24				rp2040_zero: GP10  (Pin12)
+                    b -> GP19 Pin25						 GP11  (Pin11)
    
    
-   0.01/0.1/1mm switch     -> GP26      switch to gnd
+   0.01/0.1/1mm switch     -> GP22, Pin 29      switch to gnd	GP13
+   disalbe/jogging/control -> GP21, Pin 27      switch to gnd	GP14
    
    RS485           DI  (4) -> GP0 (TX0) Pin1
                    DE  (3) -> VDD          Transceiver Output Enable
@@ -19,14 +20,16 @@ Handwheel with Encoder + Rasperry Pi Pico
 '''
 import utime
 import micropython
-from machine import Pin, I2C, Timer
+from machine import Pin, I2C, Timer, freq as CPUfreq
 from rotary import Rotary
 from ssd1306 import SSD1306_I2C
 from modbus import Modbus
 import framebuf
 
+
 micropython.opt_level(3)
 # micropython.alloc_emergency_exception_buf(100)
+CPUfreq(200_000_000)
 
 led = Pin(25, Pin.OUT)
 led.value(0)
@@ -34,7 +37,11 @@ led.value(0)
 # GPIOs Rotary Encoder
 pin_dt = 18
 pin_clk = 19
-pin_sw = 20          # switch for reset cnt, choice increment values 
+pin_sw1 = 20          # 
+pin_sw2 = 21          # jog-mode, control mode
+pin_sw3 = 22          # switch for reset cnt, choice increment values 
+
+statusline = 48
 
 # GPIOs Display 1306 with I2C
 pin_i2cclk = 9
@@ -45,11 +52,36 @@ pin_i2cdt = 8
 # use dma? see example https://github.com/raspberrypi/pico-examples/blob/master/dma/control_blocks/control_blocks.c
 i2c = I2C(0, scl=Pin(pin_i2cclk), sda=Pin(pin_i2cdt), freq=200_000)
 display = SSD1306_I2C(128,64,i2c)                           # use 1kb = 128 x 64/8
+    #  0- 47 blue
+    # 48- 63 yellow
+    
 #display.invert(True)
 display.rotate(False)
 tim = Timer()
 
-rotary = Rotary(pin_dt, pin_clk, pin_sw)                    # Init Rotary Encoder
+jogmode = 0
+displayChange = True
+buttontime = 0
+
+def sw_irq(pin):
+    global jogmode, displayChange, buttontime
+    if pin.value()==0:
+        buttontime = utime.ticks_ms()
+    dtime = utime.ticks_ms()- buttontime        
+    if pin.value()==1 and (dtime > 100 and dtime < 500):
+        if jogmode < 2:
+            jogmode +=1
+        else:
+            jogmode = 0
+        displayChange = True
+        buttontime = 0
+    print(f'     {pin.value()}, {jogmode}')
+
+rotary = Rotary(pin_dt, pin_clk, pin_sw3)                    # Init Rotary Encoder
+sw2 = Pin(pin_sw2, Pin.IN, Pin.PULL_UP)
+#sw3 = Pin(pin_sw3, Pin.IN, Pin.PULL_UP)
+sw2.irq(handler=sw_irq, trigger=Pin.IRQ_FALLING | Pin.IRQ_RISING )
+
 client = Modbus(port=0, slaveaddress=3, baudrate=38400)     # Init Modbus
 #client.debug = True
 regMemory =[0,         # absolut cnt
@@ -89,12 +121,17 @@ while True:
     if utime.ticks_us() - lasttime > 500:             
         lasttime = utime.ticks_us()
         result = client.receive()
-    if increment != old_inc:
+    if increment != old_inc or displayChange:
         old_inc = increment
         display.fill(0)
-        text = 'x0.01 mm' if increment==1 else 'x0.1 mm' if increment==10 else 'x1.0 mm'
-        display.text(text, 5, 0)
+        text = 'x0.01mm' if increment==1 else 'x0.1mm' if increment==10 else 'x1.0mm'
+        display.text(text, 1, statusline)
+        text = 'disable' if jogmode==0 else 'joggling' if jogmode==1 else 'control'
+        rotary.enable(True if jogmode==1 else False)
+        display.text(text, 72, statusline)
         display.show()
+        displayChange = False
+    
     if utime.ticks_us() - led_lasttime > 500000:
         led.toggle()
         led_lasttime = utime.ticks_us()
